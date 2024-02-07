11490a2d8713d5b5efa79b63c951d7aae0784b05
Brian Uceda <monalek0622@hotmail.com>
2024-02-06 19:34:38 -0500

    change photos

--- script.js
+++ script.js
@@ -32,32 +32,30 @@ const img = document.getElementById("img-question");
 let noIndex = 1;
 let imgIndex = 1;
 let siTextSize = 1;
-let paddingTop = 80; // va a disminuir de 5 en 5
+let paddingTop = 80;
+let textTry = 'vez';
 
 noButton.addEventListener("click", () => {
+  if (noIndex !== 1) textTry = 'veces';
+  console.log('Dijo que no ya ' + noIndex + ' ' + textTry);
+
   noButton.textContent = noModes[noIndex];
   img.src = imgModes[imgIndex];
   noIndex = (noIndex + 1) % noModes.length;
   imgIndex = (imgIndex + 1) % imgModes.length;
   siTextSize += 1;
-  if (siTextSize > 5) {
-    siTextSize += 2;
-  }
-  siButton.style.fontSize = `${siTextSize}rem`;
   paddingTop -= 5;
+
+  if (siTextSize > 5) siTextSize += 2;
+  if (siTextSize > 3) siButton.style.padding = "8px 32px";
+
+  siButton.style.fontSize = `${siTextSize}rem`;
   noContainer.style.paddingTop = `${paddingTop}px`;
-  if (siTextSize > 3) {
-    siButton.style.padding = "8px 32px";
-  }
 });
 
 siButton.addEventListener("click", () => {
   noContainer.classList.toggle("hidden");
   siContainer.classList.toggle("hidden");
-  // ponerle display none a noContainer y display flex a siContainer
-  // noContainer.style.display = "none";
-  // siContainer.style.display = "flex";
-  console.log("click");
 });
 
 // Dark mode
